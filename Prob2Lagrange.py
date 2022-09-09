1d9d39889597e16597ea2c4f8765238b79526238
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-09-09 09:26:06 +0700

    Update and rename Prob2Secant.py to Prob2Lagrange.py

---
+++ Prob2Lagrange.py
@@ -0,0 +1,5 @@
+def Lagrange(x,xi,yi):
+    #x is a floating number, eg. x=10.23
+    #xi is a tuple of x values eg. xi=(0,2/3,1)
+    #yi is a tuple of y values eg  yi=(1,0.5,0)
+    return x# return a single floating number
