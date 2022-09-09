2d466d676f7fb0c64c2ba44a90102996a8a3103d
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-09-09 09:12:29 +0700

    Initial commit

---
+++ Prob1Newton.py
@@ -0,0 +1,6 @@
+def newton(xstart):
+    x = xstart
+    # 3 root found. 0.347, -1.879, and 1.532
+    #f = lambda x: x**3 - 3*x +1 
+    #fprime = lambda x: 
+    return x
---
+++ Prob2Secant.py
@@ -0,0 +1,6 @@
+def secant(xstart1,xstart2):
+    x = xstart1
+    # 3 root found. 0.347, -1.879, and 1.532
+    #f = lambda x: x**3 - 3*x +1 
+    #fprime = lambda x: 
+    return x
